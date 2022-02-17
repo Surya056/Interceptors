78357fe8ccf09c60c5507be756184ac40e0fe39f
Surya Teja Naidu <spteja@lkqcorp.com>
2022-02-17 13:42:49 +0530

    Initial Checkin

---
+++ InterceptorConsole/InterceptorConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterceptorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
